eedd8badf62a7c942d3ac5c8dc1dd1f8c8ec4451
synthese <vyrtualsynthese@gmail.com>
2017-11-03 11:09:24 +0100

    bootstrap added

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
-  });
+      sassOptions: {
+        includePaths: [
+          'node_modules/bootstrap/scss'
+        ]
+      }
+    });
 
 
 
